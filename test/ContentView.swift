b29ff5baf877ef1dca7c250ab8860edaad99a47c
azizah110 <116996347+azizah110@users.noreply.github.com>
2022-12-29 13:10:46 +0300

    Update ContentView.swift

--- test/ContentView.swift
+++ test/ContentView.swift
@@ -1,4 +1,4 @@
-//
+
 //  ContentView.swift
 //  test
 //
@@ -8,16 +8,157 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+
+        @State private var searchText = ""
+        private var numOfImges = 2
+        private let timer = Timer.publish(every: 3, on: .main, in: .common).autoconnect()
+        @State private var CurrentImg = 0
+        
+        
+        var body: some View {
+            NavigationStack {
+                ZStack {
+                    VStack {
+                        Text("Stores")
+                            .font(.largeTitle)
+                            .fontWeight(.bold)
+                            .foregroundColor(Color(red: 0.524, green: 0.651,  blue: 0.619))
+                            .padding(.leading, 30.0)
+                            .frame(maxWidth: .infinity, alignment: .leading)
+                        
+                        
+                        
+                        HStack(spacing: 50) {
+                            NavigationLink {
+                                Text("kkkk")
+                            } label: {
+                                RoundedRectangle(cornerRadius: 40)
+                                    .fill(.white)
+                                    .frame(width: 150.0, height: 150)
+                                
+                                    .overlay(
+                                        RoundedRectangle(cornerRadius: 10)
+                                            .stroke(Color("yellow"), lineWidth:
+                                                        1))
+                                    
+                                    .overlay{
+                                        VStack {
+                                            Image("az")
+                                                .resizable()
+                                                .frame(width: 100,height: 70)
+                                            
+                                            
+                                            
+                                        }
+                                    }
+                            }
+
+                            NavigationLink {
+                                Text("kkkk")
+                            } label: {
+                                RoundedRectangle(cornerRadius: 40)
+                                    .fill(.white)
+                                    .frame(width: 150.0, height: 150)
+                                
+                                    .overlay(
+                                        RoundedRectangle(cornerRadius: 10)
+                                            .stroke(Color("yellow"), lineWidth: 1))
+                                    .overlay{
+                                        VStack {
+                                            Image("zz")
+                                                .resizable()
+                                                .frame(width: 100,height: 70)
+                                           
+                                            
+                                        }
+                                        
+                                    }
+                            }
+                        }
+                        
+                        
+                        VStack{
+                            
+                            //Spacer()
+                            
+                            Text("Services")
+                                .font(.largeTitle)
+                                .fontWeight(.bold)
+                                .foregroundColor(Color(red: 0.524, green: 0.651, blue: 0.619))
+                                .padding(30.0)
+                                .frame(maxWidth: .infinity, alignment: .leading)
+                        }.padding(.bottom, 40.0)
+                        
+    //                    HStack(spacing: 30) {
+                            
+                            
+                            //                                         Spacer()
+                            NavigationLink {
+                                Text("kkkk")
+                            } label: {
+                                RoundedRectangle(cornerRadius: 40)
+                                    .fill(.white)
+                                    .frame(width: 150.0, height: 150)
+                                
+                                
+                                    .searchable(text: $searchText)
+                                
+                                HStack(spacing: 150) {
+                                    
+                                    
+                                    VStack{
+                                        
+                                        
+                                        GeometryReader { proxy in
+                                            TabView(selection: $CurrentImg  ){
+                                                ForEach(0..<numOfImges) {num in
+                                                    Image("\(num)")
+                                                    
+                                                        .resizable()
+    //                                                    .scaledToFill()
+                                                        .tag(num)
+                                                     
+                                                    
+                                                }
+                                            }
+                                            
+                                            .tabViewStyle(PageTabViewStyle())
+                                            .clipShape(RoundedRectangle(cornerRadius: 20))
+                                            .padding()
+                                            .frame(width: proxy.size.width, height: 200)
+                                            
+                                            
+                                            
+                                            .onReceive(timer, perform: { _ in
+                                                withAnimation{
+                                                    CurrentImg = CurrentImg < numOfImges ? CurrentImg + 1 : 0
+                                                }
+                                            })
+                                        }
+                                        .padding(.top, -70.0)
+                                    }
+                                    .frame(width: 400, height: 205)
+                                    .shadow(radius: 4, x: 0, y: 3)
+                                    
+                                    
+                                    
+                                            Spacer()
+                                    
+                                    
+                                }
+                            }
+                            
+    //
+    //                        .padding(.horizontal)
+    //                        .padding(.top)
+    //                    }
+                    }
+                   
+                    
+                }
+            }
         }
-        .padding()
     }
-}
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
